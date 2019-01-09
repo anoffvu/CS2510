f75e8fb4c7f0c01aa50e26134f26b15a798440d2
anoffvu <anoffvu@gmail.com>
2019-01-09 02:16:23 -0500

    problem 1 and 2

---
+++ eclipse-workspace/Pset-1/src/GraphicNovel.java
@@ -0,0 +1,25 @@
+
+class GraphicNovel {
+  String title;
+  String author;
+  String artist;
+  int year;
+  double cost;
+  boolean monochrome;
+
+  // constructor
+  GraphicNovel(String title, String author, String artist, int year, double cost,
+      boolean monochrome) {
+    this.title = title;
+    this.author = author;
+    this.artist = artist;
+    this.year = year;
+    this.cost = cost;
+    this.monochrome = monochrome;
+  }
+}
+
+class ExamplesGraphicNovel {
+  GraphicNovel maus = new GraphicNovel("Maus", "Spiegelman", "Spiegelman", 1980, 17.60, true);
+  GraphicNovel logicomix = new GraphicNovel("Logicomix", "Doxiadis", "Papadatos", 2009, 21.00, false);
+}
---
+++ eclipse-workspace/Pset-1/src/Sundae.java
@@ -0,0 +1,33 @@
+
+interface ISundae {
+}
+
+class Scoop implements ISundae {
+  String flavor;
+
+  Scoop(String flavor) {
+    this.flavor = flavor;
+  }
+}
+
+class Topping implements ISundae {
+  ISundae inner;
+  String name;
+
+  Topping(ISundae inner, String name) {
+    this.inner = inner;
+    this.name = name;
+  }
+}
+
+class ExamplesSundae {
+  ISundae chocolateScoop = new Scoop("chocolate");
+  ISundae rainbowChocolate = new Topping(chocolateScoop, "rainbow sprinkles");
+  ISundae caramelRainbow = new Topping(rainbowChocolate, "caramel");
+  ISundae yummy = new Topping(caramelRainbow, "whipped cream");
+
+  ISundae vanillaScoop = new Scoop("Vanilla");
+  ISundae chocolateVanilla = new Topping(vanillaScoop, "chocolate sprinkles");
+  ISundae fudgeChocolate = new Topping(chocolateVanilla, "fudge");
+  ISundae noThankYou = new Topping(fudgeChocolate, "plum sauce");
+}
