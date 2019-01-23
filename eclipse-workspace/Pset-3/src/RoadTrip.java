530cf864fe37c9f51292f2ab237191205d245b35
anoffvu <anoffvu@gmail.com>
2019-01-23 17:12:33 -0500

    examples of pset 3 problem 1

--- eclipse-workspace/Pset-3/src/RoadTrip.java
+++ eclipse-workspace/Pset-3/src/RoadTrip.java
@@ -1,4 +1,53 @@
+// and predefined colors (Red, Green, Yellow, Blue, Black, White)
 
-public class RoadTrip {
+interface ILoDirection {
+
+}
+
+class ConsLoDirection implements ILoDirection {
+  Direction first;
+  ILoDirection rest;
+
+  ConsLoDirection(Direction first, ILoDirection rest) {
+    this.first = first;
+    this.rest = rest;
+  }
+}
+
+class MtLoDirection implements ILoDirection {
+  MtLoDirection() {
+  }
+}
+
+class Direction {
+  String description;
+  int miles;
+
+  Direction(String description, int miles) {
+    this.description = description;
+    this.miles = miles;
+  }
+}
+
+class RoadTrip {
+  String driver1;
+  String driver2;
+  ILoDirection directions;
+
+  RoadTrip(String driver1, String driver2, ILoDirection directions) {
+    this.driver1 = driver1;
+    this.driver2 = driver2;
+    this.directions = directions;
+  }
+}
+
+class ExamplesRoadTrip {
+  ILoDirection HazelHenryDirections = new ConsLoDirection(
+      new Direction("Make a left at Alberquerque", 13),
+      new ConsLoDirection(new Direction("Make a right at the fork", 2),
+          new ConsLoDirection(new Direction("Make a left at the next fork", 3),
+              new ConsLoDirection(new Direction("Take the overpass", 45), new ConsLoDirection(
+                  new Direction("Destination on left", 12), new MtLoDirection())))));
+  RoadTrip HazelHenryRoadTrip = new RoadTrip("Hazel", "Henry", this.HazelHenryDirections);
 
 }
