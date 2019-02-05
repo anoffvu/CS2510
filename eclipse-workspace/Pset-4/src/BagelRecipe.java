4f30a8c1af1606e41b886f47fd875abbeee33023
anoffvu <anoffvu@gmail.com>
2019-02-04 23:58:13 -0500

    copied your code

--- eclipse-workspace/Pset-4/src/BagelRecipe.java
+++ eclipse-workspace/Pset-4/src/BagelRecipe.java
@@ -1,13 +1,46 @@
+import tester.*;
+
 class BagelRecipe {
-  double flourWt;
-  double waterWt;
-  double yeastWt;
-  double saltWt;
-  double maltWt;
+  double flour;
+  double water;
+  double yeast;
+  double malt;
+  double salt;
+
+  BagelRecipe(double flour, double water, double yeast, double malt, double salt) {
+    if (flour == water) {
+      this.flour = flour;
+      this.water = water;
+    }
+    else {
+      throw new IllegalArgumentException("Invalid weight");
+    }
+
+    if (yeast == malt) {
+      this.yeast = yeast;
+      this.malt = malt;
+    }
+    else {
+      throw new IllegalArgumentException("Invalid weight");
+    }
+    if (salt + yeast == flour * 1 / 20) {
+      this.salt = salt;
+    }
+    else {
+      throw new IllegalArgumentException("Invalid weight");
+    }
+  }
+
+  BagelRecipe(double flour, double yeast) {
+    this.flour = flour;
+    this.water = flour;
+    this.yeast = yeast;
+    this.malt = yeast;
+    this.salt = (flour * 1 / 20) - yeast;
+  }
+
+  BagelRecipe(double flour, double yeast, double salt) {
+    this.flour = 
+  }
 
 }
-
-// ideal:
-// flour == water
-// yeast == malt
-// salt + yeast == 1/20 flour
