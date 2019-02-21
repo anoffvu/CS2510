74a3da977da0d4f3f41a490680040ca8d8d90daa
anoffvu <anoffvu@gmail.com>
2019-02-21 11:46:18 -0500

    tests for hit

--- eclipse-workspace/Pset-5/src/NBullets.java
+++ eclipse-workspace/Pset-5/src/NBullets.java
@@ -6,21 +6,25 @@ import javalib.funworld.WorldScene;
 import javalib.worldimages.CircleImage;
 import tester.Tester;
 
+
 class Utils {
   Random rand = new Random();
   int screenWidth = 500;
   int screenHeight = 300;
 
+  // returns a random number between the given min and max values
   public int randomNumber(int min, int max) {
     return (rand.nextInt(max - min) + min);
   }
 
+  // determines if an element is onscreen
   boolean onScreen(int radius, int x, int y) {
     return (0 - radius < x && x < screenWidth + radius)
         && (0 - radius < y && y < screenHeight + radius);
   }
 }
 
+// bullet interface 
 interface ILoBullet {
 
   // draw world
@@ -31,6 +35,7 @@ interface ILoBullet {
 
 }
 
+// represents an empty list of bullets 
 class MtLoBullet implements ILoBullet {
 
   // draws a WorldScene of an empty list of bullets
@@ -44,6 +49,7 @@ class MtLoBullet implements ILoBullet {
   }
 }
 
+// represents a list of Bullets 
 class ConsLoBullet implements ILoBullet {
   Bullet first;
   ILoBullet rest;
@@ -67,7 +73,10 @@ class ConsLoBullet implements ILoBullet {
   }
 }
 
+// ship interface 
 interface ILoShip {
+
+  // draws an ILoShip
   WorldScene draw(WorldScene ws);
 
   // moves an entire list of ships
@@ -77,6 +86,7 @@ interface ILoShip {
   public ILoShip spawn();
 }
 
+//represents an empty list of ships 
 class MtLoShip implements ILoShip {
 
   public WorldScene draw(WorldScene ws) {
@@ -99,6 +109,7 @@ class MtLoShip implements ILoShip {
   }
 }
 
+//represents a list of ships 
 class ConsLoShip implements ILoShip {
   Ship first;
   ILoShip rest;
@@ -134,8 +145,13 @@ class ConsLoShip implements ILoShip {
   }
 }
 
+//represents all the objects 
+interface IObjects {
+
+}
+
 //represents an object on the screen, either a ship or bullet 
-abstract class AObjects {
+abstract class AObjects implements IObjects {
   int size;
   int x;
   int y;
@@ -152,14 +168,34 @@ abstract class AObjects {
     this.color = color;
   }
 
+  // returns the x distance between two objects
+  public int distanceX(AObjects object) {
+    return this.x - object.x;
+  }
+
+  // returns the y distance between two objects
+  public int distanceY(AObjects object) {
+    return this.y - object.y;
+  }
+
+  // did two objects collide with eachother
+  public boolean hit(AObjects object) {
+    int x = this.distanceX(object);
+    int y = this.distanceY(object);
+
+    return Math.hypot(x, y) <= object.size + this.size;
+  }
+
   // moves an object on the screen
   public abstract AObjects move();
 
+  // determines if an object is within the screen width and height
   public boolean onScreen() {
     return new Utils().onScreen(size, x, y);
   }
 }
 
+// represents a bullet 
 class Bullet extends AObjects {
   int bulletNumber;
 
@@ -185,6 +221,7 @@ class Bullet extends AObjects {
   }
 }
 
+// represents a ship 
 class Ship extends AObjects {
 
   // this creates a random ship
@@ -193,6 +230,7 @@ class Ship extends AObjects {
     super(size, x, y, direction, velocity, color);
   }
 
+  // move a single ship across the screen
   public Ship move() {
     double x = -this.velocity * Math.cos(Math.toRadians(this.direction));
     return new Ship(this.size, (int) x + this.x, this.y, this.direction, this.velocity, this.color);
@@ -208,14 +246,13 @@ class GameScene extends World {
   public static final int screenWidth = 500;
   public static final int screenHeight = 300;
   public static final double tickRate = 1.0 / 28.0;
-  public static final int shipSpeed = 5;
-  public static final int bulletSpeed = 5;
   int bulletsLeft;
   int destroyed;
   ILoShip loShips;
   ILoBullet loBullets;
   Random random;
   int clock;
+
 
   public GameScene(int bulletsLeft, int destroyed, ILoShip loShips, ILoBullet loBullets,
       Random random, int clock) {
@@ -246,6 +283,7 @@ class GameScene extends World {
     }
   }
 
+  // if the spacebar is pressed and there is enough remaining bullets fire a bullet
   public GameScene onKeyEvent(String key) {
     if (key.equals(" ") && bulletsLeft > 0) {
       return new GameScene(this.bulletsLeft - 1, this.destroyed, this.loShips,
@@ -256,6 +294,7 @@ class GameScene extends World {
     }
   }
 
+  // draw the ships and the bullets on to the new worldscene
   public WorldScene makeScene() {
     return this.loShips.draw(this.loBullets.draw(new WorldScene(500, 300)));
   }
@@ -263,11 +302,33 @@ class GameScene extends World {
 }
 
 class ExamplesNBullets {
-  boolean testBigBang(Tester t) {
-    GameScene game = new GameScene();
-    int worldWidth = 500;
-    int worldHeight = 300;
-    double tickRate = 1.0 / 500.0;
-    return game.bigBang(worldWidth, worldHeight, tickRate);
+
+  GameScene world = new GameScene();
+
+  Bullet defaultBullet = new Bullet(2, 250, 300, 270, 8, Color.blue, 1);
+  Bullet secondBullet = new Bullet(2, 250, 292, 270, 8, Color.blue, 1);
+  Ship missShip = new Ship(20, 250, 270, 180.0, 10.0, Color.blue);
+  Ship hitShip = new Ship(20, 250, 279, 180.0, 10.0, Color.blue);
+  Ship hitShip2 = new Ship(20, 250, 285, 180.0, 10.0, Color.blue);
+
+  boolean testMove(Tester t) {
+    return t.checkExpect(this.defaultBullet.move(), new Bullet(2, 250, 292, 270, 8, Color.blue, 1))
+        && t.checkExpect(this.secondBullet.move(), new Bullet(2, 250, 284, 270, 8, Color.blue, 1));
   }
+
+  boolean testHit(Tester t) {
+    return t.checkExpect(this.defaultBullet.hit(this.hitShip), true)
+        && t.checkExpect(this.defaultBullet.hit(this.hitShip2), true)
+        && t.checkExpect(this.defaultBullet.hit(this.missShip), false);
+  }
+
+  /*
+   * boolean testBigBang(Tester t) {
+   * GameScene game = new GameScene();
+   * int worldWidth = 500;
+   * int worldHeight = 300;
+   * double tickRate = 1.0 / 500.0;
+   * return game.bigBang(worldWidth, worldHeight, tickRate);
+   * }
+   */
 }
